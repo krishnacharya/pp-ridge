94a52e655fa1c9bef646156a31ae07118f9afe67
rakshit-naidu <rakshit.naidu07@gmail.com>
2024-01-04 14:05:13 -0500

    arg parse bug #1

--- pp/synthetic_expt/specific_d_n_plevel_34_43_23.py
+++ pp/synthetic_expt/specific_d_n_plevel_34_43_23.py
@@ -89,8 +89,8 @@ def run(N, D, lambds, runs=10000):
 if __name__ == "__main__":
 
     args = argparse.ArgumentParser()
-    args.add_argument("--d", default=10, type=int)
-    args.add_argument("--n", default=100, type=int)
+    args.add_argument("d", default=10, type=int)
+    args.add_argument("n", default=100, type=int)
 
     N = [args.n]
     D = [args.d]
