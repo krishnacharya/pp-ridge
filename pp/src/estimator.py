f6d6908a856529246ae5b79b80e79c2da2802b30
rakshit-naidu <rakshit.naidu07@gmail.com>
2024-01-04 14:02:55 -0500

    non-private optimal unregularized loss

--- pp/src/estimator.py
+++ pp/src/estimator.py
@@ -16,10 +16,10 @@ def pp_estimator(epsilons, X_train, y_train, X_test, y_test, lamb, runs, eval_la
   tot_epsilon = np.sum(epsilons)
   weights_pp = epsilons / tot_epsilon # weights used in the ridge regression for personalized privacy
   sol_exact_ridge_pp = weighted_rls_solution(weights_pp, X_train, y_train, lamb) # weighted ridge on training set
-  # print("pluggin exact soln back into weighted ridge", evaluate_weighted_rls_objective(sol_exact_ridge_pp, weights_pp, X, y, lamb))
   eta_pp = compute_eta(lamb = lamb, tot_epsilon=tot_epsilon, d = d)
   # print("eta for pp", eta_pp)
   # Do runs, in each calculate loss on unweighted train, unweighted test set loss of the private estimator; 1000 runs for randomness in L2 laplce dp noise
+  print("pluggin exact soln back into weighted ridge", evaluate_weighted_rls_objective(sol_exact_ridge_pp, weights_pp, X_test, y_test, eval_lamb))  
   unweighted_train = []
   unweighted_test = []
   uniform_weight_train = np.ones(N_train) / N_train
