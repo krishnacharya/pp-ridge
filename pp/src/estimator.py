2a6a9a24a7f04bbeba9cbded9ccbfd678f753c7c
rakshit-naidu <rakshit.naidu07@gmail.com>
2024-01-04 15:50:20 -0500

    baseline changes

--- pp/src/estimator.py
+++ pp/src/estimator.py
@@ -6,13 +6,15 @@ from src.utils import weighted_rls_solution, compute_eta, compute_private_estima
 
 ## PP-ESTIMATOR
 
-def pp_estimator(epsilons, X_train, y_train, X_test, y_test, lamb, runs, eval_lamb=0):
+def pp_estimator(epsilons, X_train, y_train, X_test, y_test, lamb, runs, eval_lamb=0, baseline=False):
   '''
     X_train: np.ndarray of shape (n, d)
     epsilons: must be a numpy array of shape (len(X_train),)
   '''
   N_train, d =  X_train.shape
   N_test =  len(X_test)
+  if baseline:
+    epsilons = np.array([min(epsilons)]*N_train)
   tot_epsilon = np.sum(epsilons)
   weights_pp = epsilons / tot_epsilon # weights used in the ridge regression for personalized privacy
   sol_exact_ridge_pp = weighted_rls_solution(weights_pp, X_train, y_train, lamb) # weighted ridge on training set
@@ -24,11 +26,11 @@ def pp_estimator(epsilons, X_train, y_train, X_test, y_test, lamb, runs, eval_la
   uniform_weight_train = np.ones(N_train) / N_train
   uniform_weight_test = np.ones(N_test) / N_test
 
-  print("pluggin exact soln back into weighted ridge", evaluate_weighted_rls_objective(sol_exact_ridge_pp, uniform_weight_test, X_test, y_test, eval_lamb))
-  exact_loss_ridge = []
+  # print("pluggin exact soln back into weighted ridge", evaluate_weighted_rls_objective(sol_exact_ridge_pp, uniform_weight_test, X_test, y_test, eval_lamb))
+  # exact_loss_ridge = []
   # weighted_erm = []
   for _ in range(runs):
-    exact_loss_ridge.append(evaluate_weighted_rls_objective(sol_exact_ridge_pp, uniform_weight_test, X_test, y_test, eval_lamb))
+    # exact_loss_ridge.append(evaluate_weighted_rls_objective(sol_exact_ridge_pp, uniform_weight_test, X_test, y_test, eval_lamb))
     theta_hat_pp = compute_private_estimator(sol_exact_ridge_pp, eta_pp) # exact solution on weighted training + noise
     unweighted_train.append(evaluate_weighted_rls_objective(theta_hat_pp, uniform_weight_train, X_train, y_train, eval_lamb)) # evaluate with lambda = 0, don't add regularizer for evaluation!
     unweighted_test.append(evaluate_weighted_rls_objective(theta_hat_pp, uniform_weight_test, X_test, y_test, eval_lamb))
@@ -36,24 +38,23 @@ def pp_estimator(epsilons, X_train, y_train, X_test, y_test, lamb, runs, eval_la
 
 # JORGENSEN PRIVATE ESTIMATOR
 
-def jorgensen_private_estimator(epsilons, X_train, y_train, X_test, y_test, lamb, runs, eval_lamb=0):
+def jorgensen_private_estimator(epsilons, thresh, X_train, y_train, X_test, y_test, lamb, runs, eval_lamb=0):
   '''
-    X_train: np.ndarray of shape (n, d)
     epsilons: must be a numpy array of shape (len(X_train),)
+    thresh : np.mean(epsilons) OR max(epsilons) -- Acc to Jorgensen (using max as of now)
+    X_train: np.ndarray of shape (n, d)
     To ensure personalized privacy Jorgensen et.al first subsample from the training data acc personalized privacy levels
     then add noise
   '''
   N_train, d =  X_train.shape
   N_test =  len(X_test)
-  thresh = max(epsilons) #global threshold used in jorgensen sampling
+  # thresh = max(epsilons) #global threshold used in jorgensen sampling
   # to loop the part below
   uniform_weight_train = np.ones(N_train) / N_train
   uniform_weight_test = np.ones(N_test) / N_test
 
   unweighted_train = []
   unweighted_test = []
-
-  exact_theta_bar_loss = []
 
   for _ in range(runs):
     mask = dataset_mask_jorgensen(epsilons, thresh) # which datapoint in X_train, y_train to mask, shape (N_train)
@@ -65,8 +66,7 @@ def jorgensen_private_estimator(epsilons, X_train, y_train, X_test, y_test, lamb
     # now do DP with global threshold thresh, on the sampled data, using our framewor/sensitivity calculations
     theta_bar = weighted_rls_solution(unif_weight_samp, X_samp, y_samp, lamb) # unweighted soln with sampled data
     eta = compute_eta(lamb = lamb, tot_epsilon=tot_epsilon, d = d)
-    exact_theta_bar_loss.append(evaluate_weighted_rls_objective(theta_bar, uniform_weight_test, X_test, y_test, eval_lamb))
     theta_hat = compute_private_estimator(theta_bar, eta)
     unweighted_train.append(evaluate_weighted_rls_objective(theta_hat, uniform_weight_train, X_train, y_train, eval_lamb))
     unweighted_test.append(evaluate_weighted_rls_objective(theta_hat, uniform_weight_test, X_test, y_test, eval_lamb))
-  return np.mean(unweighted_train), np.std(unweighted_train), np.mean(unweighted_test), np.std(unweighted_test), np.mean(exact_theta_bar_loss)
+  return np.mean(unweighted_train), np.std(unweighted_train), np.mean(unweighted_test), np.std(unweighted_test)
--- pp/synthetic_expt/specific_d_n_plevel_34_43_23.py
+++ pp/synthetic_expt/specific_d_n_plevel_34_43_23.py
@@ -33,6 +33,8 @@ def run(N, D, lambds, runs=10000):
             # epsilons = epsilons_34_43_23(N_train)
             epsilons = set_epsilons(N_train, f_c=0.34, f_m=0.43, eps_c=0.01, eps_m=0.2, eps_l=1.0)
 
+            jorg_thresh = max(epsilons)
+
             Lamb = lambds
             print(f"d: {d}, n: {n}")
     
@@ -43,9 +45,12 @@ def run(N, D, lambds, runs=10000):
                 
                 # lamb = lamb * d
 
+                pp_train_mean, pp_train_std, pp_test_mean, pp_test_std = pp_estimator(epsilons, X_train, y_train, X_test, y_test, lamb, runs, eval_lamb=0)
+                _, _, pp_baseline_test_mean, pp_baseline_test_std = pp_estimator(epsilons, X_train, y_train, X_test, y_test, lamb, runs, eval_lamb=0, baseline=True)
+                jorg_train_mean, jorg_train_std, jorg_test_mean, jorg_test_std = jorgensen_private_estimator(epsilons, jorg_thresh, X_train, y_train, X_test, y_test, lamb, runs, eval_lamb=0)
                 
-                pp_unw_train_mean, pp_unw_train_std, pp_w_test_mean, pp_w_test_std, exact_loss_ridge = pp_estimator(epsilons, X_train, y_train, X_test, y_test, lamb, runs, eval_lamb=0)
-                jorg_unw_train_mean, jorg_unw_train_std, jorg_w_test_mean, jorg_w_test_std, exact_loss_jorg = jorgensen_private_estimator(epsilons, X_train, y_train, X_test, y_test, lamb, runs, eval_lamb=0)
+                jorg_thresh = min(epsilons)
+                _, _, jorg_baseline_test_mean, jorg_baseline_test_std = jorgensen_private_estimator(epsilons, jorg_thresh, X_train, y_train, X_test, y_test, lamb, runs, eval_lamb=0)
                 
                 # check_jorg_test_vals.append(jorg_w_test_mean)
 
@@ -60,17 +65,17 @@ def run(N, D, lambds, runs=10000):
                 di = {"d": d,
                     "n": n,
                     "lamb": lamb,
-                    "exact_loss_ridge": exact_loss_ridge,
-                    "exact_loss_jorg": exact_loss_jorg,
-                    "baseline": exact_loss_ridge + exact_loss_jorg,
-                    "pp_train_mean": pp_unw_train_mean,
-                    "pp_train_std": pp_unw_train_std,
-                    "pp_test_mean": pp_w_test_mean,
-                    "pp_test_std": pp_w_test_std,
-                    "jorg_train_mean": jorg_unw_train_mean,
-                    "jorg_train_std": jorg_unw_train_std,
-                    "jorg_test_mean": jorg_w_test_mean,
-                    "jorg_test_std": jorg_w_test_std}
+                    "pp_baseline_test_mean": pp_baseline_test_mean,
+                    "jorg_baseline_test_mean": jorg_baseline_test_mean,
+                    "baseline": pp_baseline_test_mean + jorg_baseline_test_mean,
+                    "pp_train_mean": pp_train_mean,
+                    "pp_train_std": pp_train_std,
+                    "pp_test_mean": pp_test_mean,
+                    "pp_test_std": pp_test_std,
+                    "jorg_train_mean": jorg_train_mean,
+                    "jorg_train_std": jorg_train_std,
+                    "jorg_test_mean": jorg_test_mean,
+                    "jorg_test_std": jorg_test_std}
                 
                 print(f"Expt {i} done, lambda {lamb}")
                 list_of_results.append(di)
@@ -99,7 +104,7 @@ if __name__ == "__main__":
     N = [args.n]
     D = [args.d]
 
-    lambds = [10, 25, 50, 75, 100, 125, 150, 175, 200, 300, 400, 500]
+    lambds = [10, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 400, 500]
     runs = 10000
 
     run(N, D, lambds, runs)
