047d832ce99ed663e0ff3bbbacc0872373729f7a
rakshit-naidu <rakshit.naidu07@gmail.com>
2024-01-10 18:39:09 -0500

    sh file bug

--- pp/real_data/california_housing/forplots_california_housing_data_impact_n_plevel_34_43_23.sh
+++ pp/real_data/california_housing/forplots_california_housing_data_impact_n_plevel_34_43_23.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
 python forplots_california_housing_data_impact_n_plevel_34_43_23.py
+exit $?
--- pp/real_data/california_housing/forplots_california_housing_data_impact_n_plevel_54_37_9.sh
+++ pp/real_data/california_housing/forplots_california_housing_data_impact_n_plevel_54_37_9.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
 python forplots_california_housing_data_impact_n_plevel_54_37_9.py
+exit $?
