8afda2abacfb6f696f5f56d5ee41c7dd31c15823
rakshit-naidu <rakshit.naidu07@gmail.com>
2024-01-10 18:22:52 -0500

    fixing housing preprocessing

--- pp/real_data/california_housing/forplots_california_housing_data_impact_n_plevel_54_37_9.py
+++ pp/real_data/california_housing/forplots_california_housing_data_impact_n_plevel_54_37_9.py
@@ -23,21 +23,18 @@ def run():
     housing = fetch_california_housing()
     
     # data (as pandas dataframes) 
-    california = pd.DataFrame(housing.data, columns=housing.feature_names)
-    y = housing.target
-
-    # numeric_all = ['Length', 'Diameter', 'Height', 'Whole_weight', 'Shucked_weight', 'Viscera_weight', 'Shell_weight']
-    # cat_all = ['Sex']
+    california = pd.DataFrame(housing.frame, columns=housing.feature_names)
     numeric_all = california.columns.to_list()
+    cat_all = []
 
     df_housing_mm = numeric_scaler(california, numeric_all) # minmax scaling for all numeric columns, so all elements in [0,1]
-    # df_medical_mm_oh = one_hot(df_housing_mm, cat_all)
-    # df_medical_mm_oh.drop(cat_all, axis = 1, inplace=True) # drop the categorics that were used to one hot encode
-    # df_medical_mm_oh = df_medical_mm_oh * 1.0 # make bool true, false into 1.0, 0.0
+    df_housing_mm = df_housing_mm * 1.0 # make bool true, false into 1.0, 0.0
     
-    X = df_housing_mm.to_numpy()
-    X = np.c_[X, np.ones(X.shape[0])] # now (n, d+1) dimensional, linear regression in d+1 is affine in d
-    
+    X = df_housing_mm.drop('MedHouseVal', axis=1)
+    X['intercept'] = 1.0
+    X = X.to_numpy() # now (n, d+1) dimensional, linear regression in d+1 is affine in d
+    y = df_housing_mm['MedHouseVal'].to_numpy()
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = seed)
     print("Training data x, y shapes", X_train.shape, y_train.shape)
     print("Test data x, y shapes", X_test.shape, y_test.shape)
