fcfe6a3b72e2f69cea1355d77977b4828ddef41d
rakshit-naidu <rakshit.naidu07@gmail.com>
2024-01-25 14:41:41 -0500

    removed notebooks folder

--- pp/real_data/california_housing/forplots_california_housing_data_impact_n_plevel_34_43_23.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python forplots_california_housing_data_impact_n_plevel_34_43_23.py
-exit $?
--- pp/real_data/california_housing/forplots_california_housing_data_impact_n_plevel_54_37_9.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python forplots_california_housing_data_impact_n_plevel_54_37_9.py
-exit $?
