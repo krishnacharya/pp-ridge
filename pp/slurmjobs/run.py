750322282eac1efe375cc2bf49be407bd70cdfb1
rakshit-naidu <rakshit.naidu07@gmail.com>
2024-05-08 18:50:20 -0400

    thetadiff plots and real lambda

--- pp/slurmjobs/run.py
+++ pp/slurmjobs/run.py
@@ -9,6 +9,81 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
+def run_real_data(runs:int, ttsplit: float, lamb:float, frac_train: float, \
+                    f_c:float, f_m:float, eps_c:float, eps_m:float, eps_l:float, seed:int):
+
+    np.random.seed(seed = seed) # set seed for data generation below, and for sklearn randomness in test train split
+    # Preprocessing
+    df_medical = pd.read_csv('../../datasets/insurance.csv')
+    numeric_all = ['age', 'bmi', 'children', 'charges']
+    cat_all = ['sex', 'smoker', 'region']
+    df_medical_mm = numeric_scaler(df_medical, numeric_all) # minmax scaling for all numeric columns, so all elements in [0,1]
+    df_medical_mm_oh = one_hot(df_medical_mm, cat_all)
+    df_medical_mm_oh.drop(cat_all, axis = 1, inplace=True) # drop the categorics that were used to one hot encode
+    df_medical_mm_oh = df_medical_mm_oh * 1.0 # make bool true, false into 1.0, 0.0
+
+    X = df_medical_mm_oh.drop('charges', axis=1)
+    X['intercept'] = 1.0
+    X = X.to_numpy() # now (n, d+1) dimensional, linear regression in d+1 is affine in d
+    y = df_medical_mm_oh['charges'].to_numpy()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = ttsplit, random_state = seed)
+    # print("Training data x, y shapes", X_train.shape, y_train.shape)
+    # print("Test data x, y shapes", X_test.shape, y_test.shape)
+
+    X_tr_frac, y_tr_frac = split_training_data(X_tr = X_train, y_tr = y_train, f = frac_train, seed = seed)
+    N_train_frac, N_test = len(X_tr_frac), len(X_test)
+    epsilons = set_epsilons(N_train_frac, f_c=f_c, f_m=f_m, eps_c=eps_c, eps_m=eps_m, eps_l=eps_l)
+
+    jorg_thresh_max, jorg_thresh_avg = np.max(epsilons), np.mean(epsilons)
+    di = {"runs":runs, "ttsplit":ttsplit, "lamb": lamb, "frac_train":frac_train, \
+        "f_c":f_c, "f_m":f_m, "eps_c":eps_c, "eps_m":eps_m, "eps_l":eps_l} # just the values for this experiment
+    # Type 1/Unregularized test loss for 1) our algorithm, 2) standard DP, 3,4) Jorgensen (max and avg thresh)
+    di['type1_nonpriv_loss'] = nonpriv_solution(N_train_frac, N_test, X_tr_frac, y_tr_frac, X_test, y_test, lamb=0, eval_lamb=0) # test loss of the non private training data solution, equal weighted, no regularization
+
+    # Our algorithm
+    di['t1_oa_train_mean'], di['t1_oa_train_std'], di['t1_oa_test_mean'], di['t1_oa_test_std'], di['t1_thetahat_oa_mean'], di['t1_thetahat_oa_std'], \
+    di['t1_thetadiff_oa_mean'], di['t1_thetadiff_oa_std']  = pp_estimator(epsilons, X_tr_frac, y_tr_frac, X_test, y_test, lamb, runs, eval_lamb=0) 
+    
+    # Standard DP
+    di['t1_sdp_train_mean'], di['t1_sdp_train_std'], di['t1_sdp_test_mean'], di['t1_sdp_test_std'], di['t1_thetahat_sdp_mean'], di['t1_thetahat_sdp_std'], \
+    di['t1_thetadiff_sdp_mean'], di['t1_thetadiff_sdp_std']  = pp_estimator(epsilons, X_tr_frac, y_tr_frac, X_test, y_test, lamb, runs, eval_lamb=0, non_personalized=True)
+
+    # Jorgensen with max and average epsilon threshold 
+    di['t1_jorgmax_train_mean'], di['t1_jorgmax_train_std'], di['t1_jorgmax_test_mean'], di['t1_jorgmax_test_std'], di['t1_thetahat_jorgmax_mean'], di['t1_thetahat_jorgmax_std'], \
+    di['t1_thetadiff_jorgmax_mean'], di['t1_thetadiff_jorgmax_std'] = jorgensen_private_estimator(epsilons, jorg_thresh_max, X_tr_frac, y_tr_frac, X_test, y_test, lamb, runs, eval_lamb=0)
+    
+    di['t1_jorgavg_train_mean'], di['t1_jorgavg_train_std'], di['t1_jorgavg_test_mean'], di['t1_jorgavg_test_std'], di['t1_thetahat_jorgavg_mean'], di['t1_thetahat_jorgavg_std'], \
+    di['t1_thetadiff_jorgavg_mean'], di['t1_thetadiff_jorgavg_std'] = jorgensen_private_estimator(epsilons, jorg_thresh_avg, X_tr_frac, y_tr_frac, X_test, y_test, lamb, runs, eval_lamb=0)
+  
+    # Type 2/ Regularized test loss for  1) our algorithm, 2) standard DP, 3,4) Jorgensen (max and avg thresh)
+
+    # Type 2
+    # Our algorithm
+    di['t2_oa_train_mean'], di['t2_oa_train_std'], di['t2_oa_test_mean'], di['t2_oa_test_std'], \
+    di['t2_thetahat_oa_mean'], di['t2_thetahat_oa_std'], \
+    di['t2_thetadiff_oa_mean'], di['t2_thetadiff_oa_std'] = pp_estimator(epsilons, X_tr_frac, y_tr_frac, X_test, y_test, lamb, runs, eval_lamb=lamb)
+
+
+     # Standard DP
+    di['t2_sdp_train_mean'], di['t2_sdp_train_std'], di['t2_sdp_test_mean'], di['t2_sdp_test_std'], \
+    di['t2_thetahat_sdp_mean'], di['t2_thetahat_sdp_std'], \
+    di['t2_thetadiff_sdp_mean'], di['t2_thetadiff_sdp_std']  = pp_estimator(epsilons, X_tr_frac, y_tr_frac, X_test, y_test, lamb, runs, eval_lamb=lamb, non_personalized=True)
+
+    # Jorgensen with max epsilon threshold 
+    di['t2_jorgmax_train_mean'], di['t2_jorgmax_train_std'], di['t2_jorgmax_test_mean'], di['t2_jorgmax_test_std'], \
+    di['t2_thetahat_jorgmax_mean'], di['t2_thetahat_jorgmax_std'], \
+    di['t2_thetadiff_jorgmax_mean'], di['t2_thetadiff_jorgmax_std'] = jorgensen_private_estimator(epsilons, jorg_thresh_max, X_tr_frac, y_tr_frac, X_test, y_test, lamb, runs, eval_lamb=lamb)
+    
+    # Jorgensen with average epsilon threshold 
+    di['t2_jorgavg_train_mean'], di['t2_jorgavg_train_std'], di['t2_jorgavg_test_mean'], di['t2_jorgavg_test_std'], \
+    di['t2_thetahat_jorgavg_mean'], di['t2_thetahat_jorgavg_std'], \
+    di['t2_thetadiff_jorgavg_mean'], di['t2_thetadiff_jorgavg_std'] = jorgensen_private_estimator(epsilons, jorg_thresh_avg, X_tr_frac, y_tr_frac, X_test, y_test, lamb, runs, eval_lamb=lamb)
+
+    return di
+
+
+
 def run_linear_synth(N:int, d:int, sigma:float, runs:int, ttsplit: float, lamb:float, frac_train: float, \
                     f_c:float, f_m:float, eps_c:float, eps_m:float, eps_l:float, seed:int) -> dict:
     '''
@@ -47,7 +122,7 @@ def run_linear_synth(N:int, d:int, sigma:float, runs:int, ttsplit: float, lamb:f
     di['t1_sdp_train_mean'], di['t1_sdp_train_std'], di['t1_sdp_test_mean'], di['t1_sdp_test_std'], di['t1_thetahat_sdp_mean'], di['t1_thetahat_sdp_std'], \
     di['t1_thetadiff_sdp_mean'], di['t1_thetadiff_sdp_std']  = pp_estimator(epsilons, X_tr_frac, y_tr_frac, X_test, y_test, lamb, runs, eval_lamb=0, non_personalized=True, theta_star=theta_star)
 
-    # Jorgmensen with max and average epsilon threshold 
+    # Jorgensen with max and average epsilon threshold 
     di['t1_jorgmax_train_mean'], di['t1_jorgmax_train_std'], di['t1_jorgmax_test_mean'], di['t1_jorgmax_test_std'], di['t1_thetahat_jorgmax_mean'], di['t1_thetahat_jorgmax_std'], \
     di['t1_thetadiff_jorgmax_mean'], di['t1_thetadiff_jorgmax_std'] = jorgensen_private_estimator(epsilons, jorg_thresh_max, X_tr_frac, y_tr_frac, X_test, y_test, lamb, runs, eval_lamb=0, theta_star=theta_star)
     
--- pp/slurmjobs/runreal-lambda.py
+++ pp/slurmjobs/runreal-lambda.py
@@ -1,13 +1,11 @@
 import pandas as pd
-from run import run_linear_synth
+from run import run_real_data
 from tqdm import tqdm
 
 # lambs = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 3, 5, 7, 10, 15, 20, 25, 50, 75, 100, 125, 150, 175, 200, 300, 400, 500, 1000]
-lambs = [0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.5, 2, 2.5, 3, 4, 5]
+lambs = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.5, 2, 2.5, 3, 4, 5, 10, 25, 50, 75, 100]
 # lambs = [1e-2, 5e-2, 1e-1, 0.5, 1, 3, 5]
 runs = 100
-d = 30
-N = 50000
 
 f_c=0.34
 f_m=0.43
@@ -24,10 +22,10 @@ tot = len(lambs)
 res = []
 with tqdm(total = tot) as pbar:
     for lamb in lambs:
-        di = run_linear_synth(N=N, d=d, sigma=0, runs=runs, ttsplit = 0.1, lamb = lamb, frac_train = 1.0, \
+        di = run_real_data(runs=runs, ttsplit = 0.1, lamb = lamb, frac_train = 1.0, \
                     f_c=f_c, f_m=f_m, eps_c=eps_c, eps_m=eps_m, eps_l=eps_l, seed=seed)
         res.append(di)
         pbar.update(1)
 
 df = pd.DataFrame(res)
-df.to_pickle('lambda-var-Rocketresults-nm2')
+df.to_pickle('lambda-var-Rocketresults-realdata-nm1')
